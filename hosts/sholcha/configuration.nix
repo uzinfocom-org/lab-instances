dbd12f662eceff956412c1d4c7c319ccccb3b6bc
wolf4am <wolf4am@gmail.com>
2026-03-30 17:10:31 +0500

    nginx update

--- hosts/sholcha/configuration.nix
+++ hosts/sholcha/configuration.nix
@@ -1,6 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 {
   config,
   lib,
@@ -141,6 +138,26 @@
     settings = {
       PasswordAuthentication = false;
       KbdInteractiveAuthentication = false;
+    };
+  };
+
+  # nginx
+  services.nginx = {
+    enable = true;
+
+    virtualHosts.localhost = {
+      listen = [
+        {
+          addr = "0.0.0.0";
+          port = 8080;
+        }
+      ];
+      locations."/" = {
+        return = "200 '<html><body>It works</body></html>'";
+        extraConfig = ''
+          default_type text/html;
+        '';
+      };
     };
   };
 
