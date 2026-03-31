54df656a5b6841eb41be758a4391eb812939d79d
wolf4am <wolf4am@gmail.com>
2026-03-31 12:26:17 +0500

    something

--- hosts/sholcha/configuration.nix
+++ hosts/sholcha/configuration.nix
@@ -9,6 +9,7 @@
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
     inputs.relago-support.nixosModules.server
+    ../../modules
   ];
 
   services.relago-server.enable = true;
@@ -44,28 +45,6 @@
   # for minecraft server
   nixpkgs.config.allowUnfree = true;
 
-  services.minecraft-server = {
-    enable = true;
-    eula = true;
-    openFirewall = true; # Opens the port the server is running on (by default 25565 but in this case 43000)
-    declarative = true;
-    whitelist = {
-      # This is a mapping of Minecraft usernames to to the players' UUIDs
-      username1 = "3e220001-9544-48bb-8fd0-ca7139727207";
-      username2 = "75b46c6c-bdbd-42ce-818e-43e94af0c043";
-    };
-    serverProperties = {
-      server-port = 25565;
-      difficulty = 3;
-      gamemode = 1;
-      max-players = 5;
-      motd = "NixOS Minecraft server!";
-      white-list = true;
-      allow-cheats = true;
-    };
-    jvmOpts = "-Xms2048M -Xmx2048M";
-  };
-
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
@@ -87,20 +66,6 @@
   #   enable = true;
   #   pulse.enable = true;
   # };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  # users.users.alice = {
-  #   isNormalUser = true;
-  #   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-  #   packages = with pkgs; [
-  #     tree
-  #   ];
-  # };
-
-  # programs.firefox.enable = true;
 
   # List packages installed in system profile.
   # You can use https://search.nixos.org/ to find more packages (and options).
---
+++ modules/services/minecraft/default.nix
@@ -0,0 +1,27 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  services.minecraft-server = {
+    enable = true;
+    eula = true;
+    openFirewall = true; # Opens the port the server is running on (by default 25565 but in this case 43000)
+    declarative = true;
+    whitelist = {
+      # This is a mapping of Minecraft usernames to to the players' UUIDs
+      username1 = "3e220001-9544-48bb-8fd0-ca7139727207";
+      username2 = "75b46c6c-bdbd-42ce-818e-43e94af0c043";
+    };
+    serverProperties = {
+      server-port = 25565;
+      difficulty = 3;
+      gamemode = 1;
+      max-players = 10;
+      motd = "NixOS Minecraft server!";
+      white-list = true;
+      allow-cheats = true;
+    };
+    jvmOpts = "-Xms2048M -Xmx2048M";
+  };
+}
