70c273384a1862ebdb218ca521c91713f241b3da
wolf4am <wolf4am@gmail.com>
2026-04-02 17:20:22 +0500

    chore: chore

--- systems/x86_64-linux/sholcha/default.nix
+++ systems/x86_64-linux/sholcha/default.nix
@@ -42,6 +42,7 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     git
     fastfetch
+    vscode
   ];
 
   # List services that you want to enable:
