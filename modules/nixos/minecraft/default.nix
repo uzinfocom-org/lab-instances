db720f5e0ca3e65f91f98265726db07560325eec
wolfram444 <kamiltox2006@gmail.com>
2026-04-24 21:05:25 +0500

    chore: minecraft server update

--- modules/nixos/minecraft/default.nix
+++ modules/nixos/minecraft/default.nix
@@ -11,12 +11,12 @@
     eula = true;
     openFirewall = true;
     declarative = true;
-    whitelist = {
-      username1 = "3e220001-9544-48bb-8fd0-ca7139727207";
-      wolf4am = "75b46c6c-bdbd-42ce-818e-43e94af0c043";
-      akmal = "8fac1980-4242-4e3f-8efc-cf7c8c969dbf";
-      let_rec = "9298739e-1971-4482-99d3-2e6020955416";
-    };
+    # whitelist = {
+    #   username1 = "3e220001-9544-48bb-8fd0-ca7139727207";
+    #   wolf4am = "75b46c6c-bdbd-42ce-818e-43e94af0c043";
+    #   akmal = "8fac1980-4242-4e3f-8efc-cf7c8c969dbf";
+    #   let_rec = "9298739e-1971-4482-99d3-2e6020955416";
+    # };
 
     serverProperties = {
       server-port = 80;
@@ -24,9 +24,10 @@
       gamemode = 1;
       max-players = 10;
       motd = "NixOS Minecraft server!";
-      white-list = true;
+      white-list = false;
       allow-cheats = true;
       view-distance = 30;
+      online-mode = false;
     };
     jvmOpts = "-Xms2048M -Xmx10G";
   };
